3aa4e2771250c3e190f5d6602852fe5a0d57ab80
LuckyWind_sck <luckywindsck@gmail.com>
2020-07-27 12:52:44 +0900

    refactor(sql): use WITH clause for filtering criteria

--- configs/logstash/pipeline/course/statement/course.sql
+++ configs/logstash/pipeline/course/statement/course.sql
@@ -1,21 +1,21 @@
-SELECT DISTINCT
-  class_info.title,
-  class_info.title_e,
-  class_info.title_memo,
-  class_info.title_memo_e,
-  class_info.year,
-  class_info.semester,
-  class_info.class_day_code,
-  class_info.class_type,
-  class_info.class_room,
-  class_info.reg_id,
-  class_info.kamoku_sort,
-  class_info.year_class_id
-FROM
-  class_info
-WHERE
-  class_info.year = 2019
-  AND class_info.katei_code IN ('11')
-  AND class_info.department IN ('23')
-  AND class_info.cur IN ('2014')
+WITH class_info AS /* Filter class_info by criteria */
+  (SELECT *
+   FROM class_info
+   WHERE class_info.year = 2019
+     AND class_info.katei_code IN ('11')
+     AND class_info.department IN ('23')
+     AND class_info.cur IN ('2014') )
+SELECT DISTINCT class_info.title,
+                class_info.title_e,
+                class_info.title_memo,
+                class_info.title_memo_e,
+                class_info.year,
+                class_info.semester,
+                class_info.class_day_code,
+                class_info.class_type,
+                class_info.class_room,
+                class_info.reg_id,
+                class_info.kamoku_sort,
+                class_info.year_class_id
+FROM class_info
 LIMIT 1;
