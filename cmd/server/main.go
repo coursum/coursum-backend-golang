6d3bcc9b75f8c23e6371319e72a0947977ffec60
LuckyWind_sck <luckywindsck@gmail.com>
2020-07-28 06:46:17 +0900

    feat(server): split code to different files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/coursum/coursum-backend/internal/app/router"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8000")
+	router.Route()
 }
---
+++ internal/app/router/main.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Route() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8000")
+}
