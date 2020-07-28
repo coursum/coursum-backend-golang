02b8e4f63bba89b164a2e10c8bdf0ee7828dd84e
LuckyWind_sck <luckywindsck@gmail.com>
2020-07-28 10:53:16 +0900

    feat(server): config cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/coursum/coursum-backend
 go 1.12
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/joho/godotenv v1.3.0
 	github.com/olivere/elastic/v7 v7.0.19
--- internal/app/router/main.go
+++ internal/app/router/main.go
@@ -2,12 +2,16 @@ package router
 
 import (
 	"github.com/coursum/coursum-backend/internal/pkg/httphandler"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // Route will setup the route for the server
 func Route() {
 	router := gin.Default()
+
+	// MiddleWare
+	router.Use(cors.Default())
 
 	// Load Template
 	router.LoadHTMLGlob("web/*.tmpl")
